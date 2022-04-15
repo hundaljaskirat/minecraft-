0711f50fb01ff67c79a61c40a84ab0b6d115c16a
hundaljaskirat <88920983+hundaljaskirat@users.noreply.github.com>
2022-04-15 13:12:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,185 +1,20 @@
-var canvas = new fabric.Canvas ("myCanvas");
-player_x = 10; 
-player_y = 10; 
-block_Image_width = 30;
-block_Image_height = 30;
-var player_object = "";
-var block_image_object = "";
-function player_update()
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
 {
-fabric.Image.fromURL("player.png",function(Img){
-player_object = Img;
-player_object.scaleToWidth(150);
-player_object.scaleToHeight(140);
-player_object.set({
-top:player_y,
-left:player_x
-
-});
-canvas.add(player_object);
-});
-
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = [images];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = [names] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
 }
-function new_image(get_image)
-{
-fabric.Image.fromURL(get_image,function(Img){
-block_image_object = Img;
-block_image_object.scaleToWidth(150);
-block_image_object.scaleToHeight(140);
-block_image_object.set({
-top:player_y,
-left:player_x
-
-});
-canvas.add(block_image_object);
-});
-
-}
-
-window.addEventListener("keydown", my_keydown);
-function my_keydown(e)
-{
-   keyPressed = e.keyCode;
-   console.log(keyPressed);
-  if(e.shiftKey == true && keyPressed =='80') 
-  {
-block_Image_width = block_Image_width + 10;
-block_Image_height = block_Image_height + 10;
-document.getElementById("current_width").innerHTML = block_mage_width;
-document.getElementById("current_height").innerHTML = block_Image_height;
-
-  }
-  if(e.shiftKey == true && keyPressed =='77') 
-  {
-block_Image_width = block_Image_width - 10;
-block_Image_height = block_Image_height - 10;
-document.getElementById("current_width").innerHTML = block_Image_width;
-document.getElementById("current_height").innerHTML = block_Image_height;
-
-  }
-  if(keyPressed == '37')
-  {
-      left();
-      console.log("left");
-  }
-  if(keyPressed == '38')
-  {
-      up();
-      console.log("up");
-  }
-  if(keyPressed == '39')
-  {
-      right();
-      console.log("right");
-  }
-  if(keyPressed == '40')
-  {
-      down();
-      console.log("down");
-  }
-  if(keyPressed == '87')
-  {
-      new_image('wall.jpg');
-      console.log("w");
-  }
-  if(keyPressed == '89')
-  {
-      new_image('yellow_wall.jpg');
-      console.log("y");
-  }
-  if(keyPressed == '82')
-  {
-      new_image('roof.jpg');
-      console.log("r");
-  }
-  if(keyPressed == '84')
-  {
-      new_image('trunk.jpg');
-      console.log("t");
-  }
-  if(keyPressed == '85')
-  {
-      new_image('unique.png');
-      console.log("u");
-  }
-  if(keyPressed == '76')
-  {
-      new_image('light_green.png');
-      console.log("l");
-  }
-  if(keyPressed == '68')
-  {
-      new_image('dark_green.png');
-      console.log("w");
-  }
-  if(keyPressed == '71')
-  {
-      new_image('ground.png');
-      console.log("g");
-  }
-  if(keyPressed == '67')
-  {
-      new_image('cloud.jpg');
-      console.log("c");
-  }
-  if(keyPressed == '79')
-  {
-      new_image('orange.jpg');
-      console.log("o");
-  }
-}
-function up()
-{
-if (player_y >=0)
-player_y = player_y - block_Image_height;
-canvas.remove(player_object);
-player_update();
-}
-function down()
-{
-if (player_y <=500)
-player_y= player_y + block_Image_height;
-canvas.remove(player_object);
-player_update();
-}
-function left()
-{
-if (player_x >=0)
-player_x = player_x - block_Image_width;
-canvas.remove(player_object);
-player_update();
-}
-function right()
-{
-if (player_x <=850)
-player_x = player_x + block_Image_width;
-canvas.remove(player_object);
-player_update();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
